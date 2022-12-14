1511c8a9f584735e9dcf9918a47370f9d54816f5
Maxime DIDIER <maxime_didier@aol.com>
2022-12-14 11:29:48 +0100

    init the project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ButtonVue from './components/Button.vue';
+const red = 'red';
+</script>
+
+<template>
+
+  <ButtonVue :color="red" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    color : {
+        required : false,
+    }
+});
+
+const active = ref(false);
+
+
+</script>
+
+<template>
+    {{props.color}}
+    {{active}}
+    <div @click="active = !active" style="background-color: red; width: 100px; height: 100px;">
+        
+    </div>
+</template>
+
+<style scoped>
+
+</style>
