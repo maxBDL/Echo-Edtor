24ca75cd65ca2e782537aa2770cb2cbc934dff86
AlbaNagisa <alban.jeux2.0@gmail.com>
2023-05-03 18:06:08 +0200

    semi spctrum

--- src/layout/Body.vue
+++ src/layout/Body.vue
@@ -1,68 +1,66 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import AsideButton from '../components/AsideButton.vue';
-import RowButton from '../components/RowButton.vue';
-import HeaderButton from '../components/HeaderButton.vue';
-import AsideSlider from '../components/AsideSlider.vue';
-import WaveSurfer from 'wavesurfer.js';
-import { AVWaveform } from 'vue-audio-visual'
-import AsideWaveForm from '../components/AsideWaveForm.vue';
+import { ref, onMounted } from "vue";
+import AsideButton from "../components/AsideButton.vue";
+import RowButton from "../components/RowButton.vue";
+import HeaderButton from "../components/HeaderButton.vue";
+import AsideSlider from "../components/AsideSlider.vue";
+import WaveSurfer from "wavesurfer.js";
+import { AVWaveform } from "vue-audio-visual";
+import AsideWaveForm from "../components/AsideWaveForm.vue";
 
-const colors = [
-    '#6054AA',
-    '#3FAE74',
-    '#D85E49',
-    '#4BBBCF'
-];
+const colors = ["#6054AA", "#3FAE74", "#D85E49", "#4BBBCF"];
 
-const emit = defineEmits(['updateGridValue']);
+const emit = defineEmits(["updateGridValue"]);
 
 // const nbButtons = ref(20);
 
 const props = defineProps({
-    audioSrc: String,
-    audioDur: Number,
-    bps: Number,
-    npb: Number
-})
+  audioSrc: String,
+  audioDur: Number,
+  bps: Number,
+  npb: Number,
+});
 
-const graphWidth = ref(1200);
+const graphWidth = ref(65500);
 
-const gridValue = ref([
-    [], [], [], []
-]);
+const gridValue = ref([[], [], [], []]);
 
 const rowAssignee = (e, index) => {
-    let row = colors.findIndex((el) => el == index);
-    gridValue.value[row] = e;
-    emit('updateGridValue', gridValue.value);
-}
-
-
+  let row = colors.findIndex((el) => el == index);
+  gridValue.value[row] = e;
+  emit("updateGridValue", gridValue.value);
+};
 </script>
 
 <template>
-    <div class="flex flex-row w-full">
-        <div class="">
-            <AsideButton v-for="i in colors.length" :number="i" />
-            <AsideWaveForm>Graphe du son</AsideWaveForm>
-        </div>
-        <div class="overflow-x-auto">
-            <RowButton v-for="color in colors" :color="color" :key="index" :nbButtons="props.audioDur*props.npb*props.bps"
-                @resultRow="rowAssignee($event, color)" />
-                <AVWaveform preload="metadata" class="w-screen" :audio-controls="false" :src="props.audioSrc" v-if="props.audioSrc != null" :canv-width="graphWidth"/>
-
-        </div>
-
+  <div class="flex flex-row w-full">
+    <div class="">
+      <AsideButton v-for="i in colors.length" :number="i" />
+      <AsideWaveForm>Graphe du son</AsideWaveForm>
     </div>
-    <div class="flex flex-row w-full">
-        <div class="">
-            
-        </div>
-        <div class="overflow-x-auto">
-        </div>
-
+    <div class="overflow-x-auto">
+      <RowButton
+        v-for="color in colors"
+        :color="color"
+        :key="index"
+        :nbButtons="props.audioDur * props.npb * props.bps"
+        @resultRow="rowAssignee($event, color)"
+      />
+      <AVWaveform
+        preload="metadata"
+        class="w-screen"
+        :audio-controls="false"
+        :src="props.audioSrc"
+        v-if="props.audioSrc != null"
+        :canv-width="graphWidth"
+      />
+      {{ graphWidth }}
     </div>
+  </div>
+  <div class="flex flex-row w-full">
+    <div class=""></div>
+    <div class="overflow-x-auto"></div>
+  </div>
 </template>
 
 <style></style>
